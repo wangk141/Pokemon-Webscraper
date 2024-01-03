03879709c9ca052a91b561c37a9836b074e04df9
wangk141 <97752344+wangk141@users.noreply.github.com>
2024-01-02 23:58:35 -0700

    Main Functionality
    Combined 3 separately tested functions

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+import mysql.connector
 from selenium.webdriver.chrome.options import Options
 import pandas as pd
+import glob
+import csv
+
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 def steal_data(url_input: str, set_name: str) -> None:
@@ -41,7 +49,75 @@ def steal_data(url_input: str, set_name: str) -> None:
         print("set omitted: " + set_name)
 
 
-# WE HAVE HEADLESS MODE
+def combine_path(filepath: str) -> list[list[str, str, str, str]]:
+    """
+    Combines multiple csv files into one large csv file to make it easier to load into sql
+    database
+
+    filepath: string representation of filepath
+    """
+    rows = []
+    # Recursively searches all elements that match
+    for name in glob.glob(filepath):
+        with open(name, newline='') as csvfile:
+            # Can change delimiter here to spaces or whatever, spell it correctly
+            csvreader = csv.reader(csvfile, delimiter=',')
+            counter = 0
+            for row in csvreader:
+                if counter == 0:
+                    pass
+                else:
+                    rows.append([row[0], row[1], row[2], row[3]])
+                counter += 1
+    return rows
+
+
+def search_raw(input_url: str, lst: list[list[str, str, str, str]]) -> list[list[str, str, str, str, str]]:
+    """
+
+    :param input_url:
+    :param lst:
+    :return:
+    """
+    for line in lst:
+        # for each name in list, search card name on google, look for tcgplayer link
+        # click tcgplayer link, wait for resulting price
+        chrome_options = Options()
+        chrome_options.add_argument("--headless=new")  # for Chrome >= 109
+        driver = webdriver.Chrome(options=chrome_options)
+        driver.get("https://www.google.ca/")
+        search = driver.find_element(By.NAME, "q")
+        search.send_keys(line[0])
+        search.send_keys(Keys.RETURN)
+
+        try:
+            # Wait for the search results container to be present in the DOM
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.ID, 'search'))
+            )
+
+            # Assuming you are looking for a link to a specific website in the search results
+            # Replace 'Your Target Website' with the actual name or URL of the website you are waiting for
+            target_website_link = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, input_url))
+            )
+
+            # Click on the link to the target website
+            target_website_link.click()
+            find = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "price"))
+            )
+            line.append(find.text[1:])
+            print(line)
+        except:
+            print("some error occurred")
+            line.append('0.00')
+        finally:
+            driver.quit()
+    return lst
+
+
+# Web scrape all pokemon sets into csv files
 chrome_options = Options()
 chrome_options.add_argument("--headless=new")  # for Chrome >= 109
 driver = webdriver.Chrome(options=chrome_options)
@@ -60,3 +136,21 @@ for a_set in all_sets:
     except:
         print("missing set: " + a_set)
 
+myurl = 'C:/Users/KAIXI/PycharmProjects/WebScraper/csvfiles/*.csv'
+search_raw("tcgplayer.com", combine_path(myurl))
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="readinghorsecrackspizza",
+    database="pokemondb"
+)
+
+a = combine_path('C:/Users/KAIXI/PycharmProjects/WebScraper/csvfiles/*.csv')
+data = search_raw("tcgplayer.com", a)
+print(data)
+query = "INSERT INTO `pokemondb`(`Name`, `PSA9`, `PSA10`, `SETNAME`, `RAW`) VALUES (%s, %s, %s, %s, %s)"
+
+mymouse = mydb.cursor()
+mymouse.executemany(query, data)
+mydb.commit()
